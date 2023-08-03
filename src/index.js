d813d8089f7a4b20635a11266563fcd13d6ae3f7
Ioana Caraman <ioanacaraman3@gmail.com>
2023-08-03 09:54:20 +0300

    initial commit - Tic-Tac-Toe JavaScript Final

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let currentPlayer = 'x';
 const winningCombinations = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
 
 const cellElements = document.getElementsByClassName("cell")
-for (let i = 0; i < cellElements.length; i++) {
+for (let i = 0; i < cellElements.length; ++i) {
     cellElements[i].addEventListener('click', startGame, false);
 }
 
@@ -39,7 +39,7 @@ let fullCells = document.querySelectorAll("[data-player]").length;
 }
 
 function resetGame() {
-    for (let i = 0; i < cellElements.length; i++) {
+    for (let i = 0; i < cellElements.length; ++i) {
         cellElements[i].innerText = '';
         cellElements[i].removeAttribute('data-player');
     }
