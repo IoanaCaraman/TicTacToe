6ae03d87fea130a0f9541be55de49c77092a9d0e
Ioana Caraman <ioanacaraman3@gmail.com>
2023-08-04 12:26:09 +0300

    initial commit - Tic-Tac-Toe JavaScript after codereview

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-window.addEventListener("DOMContentLoaded", (event) => {
 let currentPlayer = 'x';
 const winningCombinations = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
 
@@ -7,34 +6,33 @@ for (let i = 0; i < cellElements.length; ++i) {
     cellElements[i].addEventListener('click', startGame, false);
 }
 
-let restartButton = document.getElementById('restartButton');
-restartButton.addEventListener('click', resetGame);
-
 function startGame() {
-if (this.innerText === '') {
-   this.innerText = currentPlayer;
-   this.setAttribute('data-player', currentPlayer);
-}
+    if (this.innerText === '') {
+        this.innerText = currentPlayer;
+        this.setAttribute('data-player', currentPlayer);
+    }
 
-checkWin();
-if (currentPlayer === 'x') {
-    currentPlayer = '0';
+    checkWin();
+    if (currentPlayer === 'x') {
+        currentPlayer = '0';
     } else {
-    currentPlayer = 'x'
+        currentPlayer = 'x'
     }
 }
 
 function checkWin() {
-let checkCells = document.querySelectorAll('[data-player="' + currentPlayer + '"]');
-let valueCells = Array.from(checkCells, cell => cell.getAttribute('data-cell'));
-let isFounded = winningCombinations.some(ai => {
-   return ai.every(cell => valueCells.includes(cell));
-});
-let fullCells = document.querySelectorAll("[data-player]").length;
+    let checkCells = document.querySelectorAll('[data-player="' + currentPlayer + '"]');
+    let valueCells = Array.from(checkCells, cell => cell.getAttribute('data-cell'));
+    let isFounded = winningCombinations.some(ai =>
+    {
+        return ai.every(cell => valueCells.includes(cell));
+    });
+    let fullCells = document.querySelectorAll("[data-player]").length;
+
     if (isFounded == true) {
-      winnerMessage.innerText = currentPlayer + " " + "won this game!";
+        winnerMessage.innerText = currentPlayer + " " + "won this game!";
     } else if (fullCells == 9) {
-     winnerMessage.innerText = "Equality between players!";
+        winnerMessage.innerText = "Equality between players!";
     }
 }
 
@@ -43,7 +41,7 @@ function resetGame() {
         cellElements[i].innerText = '';
         cellElements[i].removeAttribute('data-player');
     }
-currentPlayer = 'x';
-winnerMessage.innerText = '';
+
+    currentPlayer = 'x';
+    winnerMessage.innerText = '';
 }
-});
